d6c6eae260ec256e8cf35ef90c1656435a6244a4
iwasakiyosuke5 <iwasakiyosuke5@gmail.com>
2024-10-09 19:04:14 +0900

    マイページも検索可能

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -7,9 +7,26 @@ use App\Models\kaizenProposal;
 
 class MypageController extends Controller
 {
-    public function create()
+    public function create(Request $request)
     {
-        $posts = kaizenProposal::where('user_id',auth()->id())->orderBy('idKP','desc')->paginate(5);
+        $query = kaizenProposal::query();
+
+        if ($request->filled('search')) {
+            $search = $request->input('search');
+            $keywords = preg_split('/[ \x{3000}]+/u', $search, -1, PREG_SPLIT_NO_EMPTY);
+
+            $query->where(function ($q) use ($keywords) {
+                foreach ($keywords as $keyword) {
+                    $q->where(function ($subQuery) use ($keyword) {
+                        $subQuery->where('title', 'like', '%' . $keyword . '%')
+                        ->orWhere('approvalStage', 'like', '%' . $keyword . '%');
+                    });
+                }
+            });
+        }
+
+
+        $posts = $query->where('user_id',auth()->id())->orderBy('idKP','desc')->paginate(5);
         return view('mypage',compact('posts'));
     }
 }
